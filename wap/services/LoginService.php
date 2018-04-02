826d1dfb4191a97acadddce6a2ad6f8b8cb18379
fang1993 <1041013076@qq.com>
2018-04-02 21:33:51 +0800

    login
    登录

--- wap/services/LoginService.php
+++ wap/services/LoginService.php
@@ -18,16 +18,26 @@ class LoginService extends BaseService
 
     public function index($data){
 
-        $user=User::find()->where(['name'=>$data['name']])->asArray()->one();
-        if(!$user){
-            throw new \Exception('用户不存在');
+        try {
+            if (!isset($data['name']) || !$data['name']) {
+                throw new \Exception('用户名未提交');
+            }
+            if (!isset($data['password']) || !$data['password']) {
+                throw new \Exception('密码未提交');
+            }
+            $user = User::find()->where(['name' => $data['name']])->asArray()->one();
+            if (!$user) {
+                throw new \Exception('用户不存在');
+            }
+            if (md5($data['password']) != $user['password']) {
+                throw new \Exception('密码错误');
+            }
+            \Yii::$app->session->set('user_id', $user['user_id']);
+            setcookie('user', Json::encode($user), 0, '/');
+        }catch (\Exception $e){
+            return Json::encode(['status'=>1,'msg'=>'登录成功']);
         }
-        if(md5($data['password'])!=$user['password']){
-            throw new \Exception('密码错误');
-        }
-        \Yii::$app->session->set('user_id',$user['user_id']);
-        setcookie('user',Json::encode($user),0,'/');
-        return Json::encode(['status'=>1,'msg'=>'success','data'=>$data]);
+        return Json::encode(['status'=>0,'msg'=>'登录失败']);
     }
 }
 
