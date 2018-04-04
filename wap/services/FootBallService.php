8aca0cc598288d4f63d3c7dbd61f9fc5a6574cf4
fang1993 <1041013076@qq.com>
2018-04-04 21:46:39 +0800

    add-o

--- wap/services/FootBallService.php
+++ wap/services/FootBallService.php
@@ -13,6 +13,7 @@ use common\models\BallMatch;
 use common\models\MatchInfo;
 use common\models\Order;
 use common\models\User;
+use yii\db\Expression;
 use yii\helpers\Json;
 
 class FootBallService extends BaseService
@@ -48,20 +49,29 @@ class FootBallService extends BaseService
 
     public function buy($data)
     {
+        $transaction=\Yii::$app->db->beginTransaction();
         try {
+            $user_id=1;//@todo 暂时写死
+            $user=User::findOne($user_id);
+            if($user->fee<$data['buy_money']){
+                throw new \Exception('您账户余额不足');
+            }
+            User::updateAll(['fee'=>new Expression("fee-{$data['buy_money']}")],['user_id'=>$user_id]);
             $order = new Order();
             $order->ball_match_id = $data['ball_match_id'];
             $order->buy_result = $data['buy_result'];
             $order->buy_money = $data['buy_money'];
             $order->get_money = $data['money'];
             $order->charge = $data['charge'];
-            $order->user_id = 1;//@todo 暂时写死
+            $order->user_id = $user_id;
             $order->match_info_id = $data['match_info_id'];
             $order->create_time = time();
             if (!$order->validate()) {
                 throw new \Exception(Json::encode($order->getErrors()));
             }
+            $transaction->commit();
         }catch (\Exception $e){
+            $transaction->rollBack();
             return Json::encode(['status'=>0,'msg'=>$e->getMessage()]);
         }
         return Json::encode(['status'=>1,'msg'=>'success']);
