58da93a9fe24d9cdab1eb6fe5fb1979c3b57ea8f
fang1993 <1041013076@qq.com>
2018-04-05 12:20:24 +0800

    user

--- wap/controllers/UserController.php
+++ wap/controllers/UserController.php
@@ -3,6 +3,7 @@ namespace wap\controllers;
 use wap\services\FootBallService;
 use wap\services\MessageService;
 use wap\services\UserService;
+use yii\helpers\Json;
 
 
 /**
@@ -16,8 +17,13 @@ class UserController extends BaseController
     }
 
     public function actionAdd(){
-        $data=\Yii::$app->request->post();
-        return UserService::service()->add($data);
+        if(\Yii::$app->request->isPost){
+            $data=\Yii::$app->request->post();
+            return UserService::service()->add($data);
+        }else{
+            return Json::encode(['status'=>1,'msg'=>'success']);
+        }
+
     }
 
 }
