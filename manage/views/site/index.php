96ee7ad1463f81068e87f0fdf19d83f8686301c4
fang1993 <1041013076@qq.com>
2018-04-07 17:53:09 +0800

    img

--- manage/views/site/index.php
+++ manage/views/site/index.php
@@ -1,1 +1,4 @@
-<?php echo "this is site index";
+<div class="_page">
+    <img src="/images/1zh.jpg" style="height: 100%;width: 100%">
+</div>
+<script type="text/javascript" src="/js/app/order/index.js"></script>
